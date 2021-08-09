6e75a59ef611fe8111c617719320afd0314bf5b9
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-08-09 10:45:06 +0530

    Fixed "colorless web-devicons in telescope window"

--- lua/gruvqueen/util.lua
+++ lua/gruvqueen/util.lua
@@ -32,10 +32,8 @@ function M.modify(default, user_config)
 end
 
 function M.set_default()
-    vim.api.nvim_command("hi clear")
-    if vim.fn.exists("syntax_on") then
-        vim.api.nvim_command("syntax reset")
-    end
+    if vim.g.colors_name then vim.cmd("hi clear") end
+    if vim.fn.exists("syntax_on") then vim.cmd("syntax reset") end
     vim.o.termguicolors = true
     vim.g.colors_name = "gruvqueen"
 end
